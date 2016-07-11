8ccdf9f14ddea351526f807605d9f76fa6f57b85
Greg Brunk <greg.brunk@gmail.com>
2016-07-11 10:53:39 -0600

    Wrote getAll routes (front and back), populated seed file, setup mongo server (finished step 1)

--- public/js/app.js
+++ public/js/app.js
@@ -5,46 +5,12 @@
  *
  */
 
-
-/* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
-/* end of hard-coded data */
-
-
-
-
 $(document).ready(function() {
   console.log('app.js loaded!');
-
+  $.get("/api/albums", function(data, status){
+        data.forEach(renderAlbum);
+    });
 });
-
-
-
-
 
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
@@ -52,7 +18,7 @@ function renderAlbum(album) {
 
   var albumHtml =
   "        <!-- one album -->" +
-  "        <div class='row album' data-album-id='" + "HARDCODED ALBUM ID" + "'>" +
+  "        <div class='row album' data-album-id='" + album.artistName + "'>" +
   "          <div class='col-md-10 col-md-offset-1'>" +
   "            <div class='panel panel-default'>" +
   "              <div class='panel-body'>" +
@@ -65,15 +31,15 @@ function renderAlbum(album) {
   "                    <ul class='list-group'>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Album Name:</h4>" +
-  "                        <span class='album-name'>" + "HARDCODED ALBUM NAME" + "</span>" +
+  "                        <span class='album-name'>" + album.name + "</span>" +
   "                      </li>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Artist Name:</h4>" +
-  "                        <span class='artist-name'>" +  "HARDCODED ARTIST NAME"+ "</span>" +
+  "                        <span class='artist-name'>" +  album.artistName + "</span>" +
   "                      </li>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Released date:</h4>" +
-  "                        <span class='album-releaseDate'>" + "HARDCODED ALBUM RELEASE" + "</span>" +
+  "                        <span class='album-releaseDate'>" + album.releaseDate + "</span>" +
   "                      </li>" +
   "                    </ul>" +
   "                  </div>" +
@@ -90,5 +56,6 @@ function renderAlbum(album) {
   "          <!-- end one album -->";
 
   // render to the page with jQuery
+  $('#albums').append(albumHtml);
 
 }
--- seed.js
+++ seed.js
@@ -4,16 +4,37 @@
 var db = require("./models");
 
 var albumsList =[
-  // put data here!
+	{
+	artistName: 'dat Old Kanye',
+	name: 'The College Dropout',
+	releaseDate: '2004, February 10',
+	genres: [ 'rap', 'hip hop' ]
+	},
+	{
+	artistName: 'dat New Kanye',
+	name: 'The Life of Pablo',
+	releaseDate: '2016, Febraury 14',
+	genres: [ 'hip hop' ]
+	},
+	{
+	artistName: 'dat always rude Kanye',
+	name: 'My Beautiful Dark Twisted Fantasy',
+	releaseDate: '2010, November 22',
+	genres: [ 'rap', 'hip hop' ]
+	},
+	{
+	artistName: 'dat sweet Kanye',
+	name: '808s & Heartbreak',
+	releaseDate: '2008, November 24',
+	genres: [ 'r&b', 'electropop', 'synthpop' ]
+	}
 ];
 
 db.Album.remove({}, function(err, albums){
-
   db.Album.create(albumsList, function(err, albums){
     if (err) { return console.log('ERROR', err); }
     console.log("all albums:", albums);
     console.log("created", albums.length, "albums");
     process.exit();
   });
-
 });
